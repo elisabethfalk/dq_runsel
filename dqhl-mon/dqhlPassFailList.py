9f602de3199d83a5db90930fc18e0b8bc3e89adf
Elisabeth Falk <E.Falk@sussex.ac.uk>
2017-05-29 13:54:16 +0100

    Adapted to removed DQHL processor checks

--- dqhl-mon/dqhlPassFailList.py
+++ dqhl-mon/dqhlPassFailList.py
@@ -28,23 +28,23 @@ def initRunlistFile(firstRun, lastRun):
     runlistFile.write("\n")
     runlistFile.write("Run no |   By processor   |" + \
                       "    Trigger Processor    |" + \
-                      "                 Time Processor                 |" + \
+                      "             Time Processor              |" + \
                       "       Run Processor       |   PMT Processor\n")
     runlistFile.write("---------------------------" + \
                       "--------------------------" + \
-                      "--------------------------------------------------" + \
+                      "-------------------------------------------" + \
                       "-----------------------------------------------\n")
     runlistFile.write("       | TTRP    | TTRP   |" + \
                       " N100L ESUMH Miss BitFlp |" + \
-                      " Event GT in  Re-   1st ev 10 MHz  Event Event  |" + \
-                      " Physics Monte Run    Trig | Ov'all Crate Panel\n")
+                      " Event GT in  Re-   1st ev 10 MHz  Event |" + \
+                      " Physics Monte Trig | (Run | Ov'all Crate Panel\n")
     runlistFile.write("       | (modif) | (orig) |"+ \
                       " rate  rate  GTID GTID   |" + \
-                      " rate  oth ev trigs time   UT comp order sep UT |" + \
-                      " run     Carlo length mask | covg   covg  covg\n")
+                      " rate  oth ev trigs time   UT comp order |" + \
+                      " run     Carlo mask | len) | covg   covg  covg\n")
     runlistFile.write("---------------------------" + \
                       "--------------------------" + \
-                      "--------------------------------------------------" + \
+                      "-------------------------------------------" + \
                       "-----------------------------------------------\n")
 
     return runlistFile
@@ -66,7 +66,7 @@ def processRun(runNumber, data, runlistFile):
            modifTriggerProcChecksOK(triggerProc), \
            modifTimeProcChecksOK(timeProc), \
            modifRunProcChecksOK(runProc), pmtProcChecksOK(pmtProc), \
-           triggerProcChecksOK(triggerProc), timeProcChecksOK(timeProc), \
+           nominalTriggerProcChecksOK(triggerProc), timeProcChecksOK(timeProc), \
            runProcChecksOK(runProc), pmtProcChecksOK(pmtProc)) + \
                       " %i     %i     %i    %i      |" % \
            (triggerProc['n100l_trigger_rate'], \
@@ -76,41 +76,47 @@ def processRun(runNumber, data, runlistFile):
                       " %i     %i      %i     %i     " % \
            (timeProc['event_rate'], timeProc['event_separation'], \
            timeProc['retriggers'], timeProc['run_header']) + \
-                      " %i       %i     %i      |" % \
-           (timeProc['10Mhz_UT_comparrison'], timeProc['clock_forward'], \
-           timeProc['delta_t_comparison']) + \
-                      " %i       %i     %i      %i    | %i      %i     %i\n" % \
+                      " %i       %i     |" % \
+           (timeProc['10Mhz_UT_comparrison'], timeProc['clock_forward']) + \
+                     " %i       %i     %i    |  %i   | %i      %i     %i\n" % \
            (runProc['run_type'], runProc['mc_flag'], \
-           runProc['run_length'], runProc['trigger'], \
+           runProc['trigger'], runProc['run_length'], \
            pmtProc['general_coverage'], pmtProc['crate_coverage'], \
            pmtProc['panel_coverage']))
 
     return
 
 def dqhlPassFailList(firstRun, lastRun):
-
     # Open and initiate run-list file:
     runlistFile = initRunlistFile(firstRun, lastRun)
 
-    # Loop over run range to print stats and fill histogram:
-    i = 0
+    # Open database(s):
+    db = couchdb.Server(DB_settings.COUCHDB_SERVER)
+
+    # Loop over run range to extract stats and fill list(s)/histogram(s):
+    nRuns = 0
     for runNumber in range(firstRun, lastRun+1):
         # if ((i % 10 == 0) and (i != 0)):
-        if ((runNumber % 10 == 0) and (i != 0)):
+        if ((runNumber % 10 == 0) and (nRuns != 0)):
             runlistFile.write("- - - -|- - - - -|- - - - |" + \
                               " - - - - - - - - - - - - |" + \
                               " - - - - - - - - - - - - - "
-                              "- - - - - - - - - - -|" + \
-                              "- - - - - - - - - - - - - -|- - - - - - - - - -\n")
+                              "- - - - - - - | - - - - - - - - - - - - - |" + \
+                              " - - - - - - - - - \n")
 
         # Download DQ ratdb table:
-        db = couchdb.Server(DB_settings.COUCHDB_SERVER)
         data = getCouchDBDict(db, runNumber)
         # print "data: ", data
-        if (data != None):
-            print "Processing DQHL record for run number %i" % runNumber
-            processRun(runNumber, data, runlistFile)
-            i += 1
+        if (data is not None):
+            if isPhysicsRun(data):
+                print "Processing DQHL record for run number %i" % runNumber
+                processRun(runNumber, data, runlistFile)
+                nRuns += 1
+            else:
+                print "Run number %i is not a PHYSICS run" % runNumber + \
+                      " (although DQHL record was found)"
+        else:
+            print "No DQHL record found for run number %i" % runNumber
 
     return
 
